aad944bd044e5232fc3401d800fd8e42381201d7
YeMacro·Lee <411044722@qq.com>
2019-11-14 17:08:01 +0800

    提交本地仓库

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,45 @@
+package com.itheima.pojo;
+
+/**
+ * 作者:YeMacro·Lee
+ * 日期:2019-11-14  12:55:33
+ */
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public User() {
+
+
+    }
+}
